2d260107f933b6076bebed51bc3168e500f8fd32
Chris Bongers <chrisbongers@gmail.com>
2022-04-14 21:06:08 +0200

    feat: Data loading added

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,34 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getPosts } from "~/models/post.server";
+
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader = async () => {
+  return json<LoaderData>({
+    posts: await getPosts(),
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData() as LoaderData;
+  return (
+    <main>
+      <h1>All my posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link
+              to={post.slug}
+              className="text-blue-600 underline"
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
