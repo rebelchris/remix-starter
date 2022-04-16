fe32e72aa87fda46300a796537a6450a75d59123
Chris Bongers <chrisbongers@gmail.com>
2022-04-16 08:06:24 +0200

    feat: add Prisma as the data source

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,22 +1,9 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { prisma } from "~/db.server";
 
-export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "post-2",
-      title: "My second post",
-    },
-  ];
+export async function getPosts() {
+  return prisma.post.findMany();
 }
 
-export async function getPost(slug: string | undefined): Promise<Post> {
-  const posts = await getPosts();
-  return posts.filter(post => post.slug === slug)[0]
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
 }
