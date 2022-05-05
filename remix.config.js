99ec442971c33b6176f133ffcdc427412226ce0f
Chris Bongers <chrisbongers@gmail.com>
2022-05-05 09:10:16 +0200

    feat: add markdown toc plugin

--- remix.config.js
+++ remix.config.js
@@ -4,4 +4,14 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  mdx: async (filename) => {
+    const [rehypeSlug, rehypeAutolinkHeadings, rehypeToc] = await Promise.all([
+      import('rehype-slug').then((mod) => mod.default),
+      import('rehype-autolink-headings').then((mod) => mod.default),
+      import('rehype-toc').then((mod) => mod.default),
+    ]);
+    return {
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeToc],
+    };
+  },
 };
